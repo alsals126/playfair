d2c2bead1b189db7e5d9657baec9e1b9089fba24
오정민 <9970mia@naver.com>
2021-04-28 14:27:43 +0900

    210428

--- encryptionAll.js
+++ encryptionAll.js
@@ -1,22 +1,15 @@
-var keyword = ""
-var plaintext = ""
+var keyword = ""    //암호화에 쓰일 키
+var plaintext = ""  //암호화 할 문자열
 
-function getValue()  {
+function getValue()  { // 암호화에 쓰일 키, 암호화 할 문자열 가져오기
   localStorage.setItem('keyword', document.getElementById('keyword').value)
   localStorage.setItem('plaintext', document.getElementById('plaintext').value)
 }
 
 function encryption(){
-  // 암호키 중복 제거
-  var keyForSet = localStorage.getItem('keyword')
-  console.log(deduplication(keyForSet))
-
-  // 암호판 중복 제거
   var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  uniqueArr += alphabet
-  console.log(uniqueArr)
-  
-  
+  var keyForSet = deduplication(localStorage.getItem('keyword').toUpperCase() + alphabet) //중복된 문자가 제거된 문자열을 저장할 변수
+  console.log(keyForSet)  
 }
 
 function deduplication(word){ //중복제거
@@ -25,4 +18,8 @@ function deduplication(word){ //중복제거
   Array.isArray(result)
   result = result.join('').toUpperCase()
   return result
+}
+
+function setBoard(){ //암호화에 쓰일 암호판 세팅
+  const alphabetBoard = Array.from(Array(5), () => new Array(5)) //빈 배열 생성
 }
